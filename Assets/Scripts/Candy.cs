c670eebae7c244b459776763a5efc2c0dff1128b
Y3st5 <juandiex2005@gmail.com>
2025-10-16 15:56:25 -0500

    GA

--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Caramelo : MonoBehaviour
+public class Candy : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
